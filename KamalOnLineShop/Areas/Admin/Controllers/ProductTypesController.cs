01a24f9f725be5b7d953b156f673c00177c44192
dell <dell@DESKTOP-A67SHSR>
2022-05-26 00:25:18 +0400

    Add project files.

---
+++ KamalOnLineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,42 @@
+﻿using KamalOnLineShop.Data;
+using KamalOnLineShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KamalOnLineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.ProductTypes.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(ProductTypes model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ProductTypes.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+    }
+}
