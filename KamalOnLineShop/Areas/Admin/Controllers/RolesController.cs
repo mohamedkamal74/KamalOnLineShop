984cacb546f0224c333bdc22dda08d8af0aa6490
mohamed kamal <mkahf5103@gmail.com>
2022-06-02 19:44:03 +0400

    add assign method and views to roles controller

--- KamalOnLineShop/Areas/Admin/Controllers/RolesController.cs
+++ KamalOnLineShop/Areas/Admin/Controllers/RolesController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using KamalOnLineShop.Areas.ViewModels;
+using KamalOnLineShop.Data;
+using KamalOnLineShop.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +13,15 @@ namespace KamalOnLineShop.Areas.Admin.Controllers
     public class RolesController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly UserManager<IdentityRole> _userManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
         public RolesController(RoleManager<IdentityRole>roleManager,
-            UserManager<IdentityRole>userManager)
+            UserManager<IdentityUser>userManager,ApplicationDbContext context)
         {
             _roleManager = roleManager;
            _userManager = userManager;
+            _context = context;
         }
         public IActionResult Index()
         {
@@ -132,8 +138,31 @@ namespace KamalOnLineShop.Areas.Admin.Controllers
 
         public async Task<IActionResult> Assign()
         {
-            ViewBag.userId = _userManager.Users.ToList();
-            ViewBag.roleId = _roleManager.Roles.ToList();
+            ViewBag.UserId = _context.ApplicationUsers.Where(x=>x.LockoutEnd<DateTime.Now||x.LockoutEnd==null).ToList();
+            ViewBag.RoleId = _roleManager.Roles.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Assign(RoleUserVM roleuser)
+        {
+            var user=_context.ApplicationUsers.FirstOrDefault(x=>x.Id==roleuser.UserId);
+            var checkisroleassign =await _userManager.IsInRoleAsync(user, roleuser.RoleId);
+            if (checkisroleassign)
+            {
+                ViewBag.msg = "this user already assigned in this role";
+                ViewBag.UserId = _context.ApplicationUsers.Where(x => x.LockoutEnd < DateTime.Now || x.LockoutEnd == null).ToList();
+                ViewBag.RoleId = _roleManager.Roles.ToList();
+                return View();
+
+            }
+            var role= await _userManager.AddToRoleAsync(user, roleuser.RoleId);
+            if (role.Succeeded)
+            {
+                TempData["save"] = "User Role Assigned Succesufully";
+                return RedirectToAction(nameof(Index));
+            }
             return View();
         }
     }
